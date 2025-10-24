f4f4748f6876320f122209ca1ca914802bffaee7
bienvenudev <cbienvenu007@gmail.com>
2025-10-24 19:07:33 +0200

    refactor: update Card component to use Tailwind's built-in classes

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,11 @@ function App() {
   return (
     <div className="min-h-[100vh] grid place-items-center">
       <div className="grid grid-rows-2 gap-4">
-        <div className="flex items-center gap-4 justify-center">
+        <div className="flex gap-4 justify-center">
           {/* <p className="uppercase mr-4">1</p> */}
           <Card icon={<IoCloudUpload className='size-12'/>} iconBackgroundColor='#3F75FE' title='Easy Deployment' text='Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.' />
-          <Card icon={<IoAccessibility className='size-12'/>} iconBackgroundColor='#2E21FF' title='Accessibility Features' text='Our platform is designed to be inclusive and accessible for all users.' />
-        </div>
-
-        <div className="flex items-center gap-4 justify-center">
-          <p className="uppercase mr-4">2</p>
-
+          <Card icon={<IoAccessibility className='size-12'/>} iconBackgroundColor='#2E21FF' title='Accessibility Features' text='Our platform is designed to be inclusive and accessible for all users.'/>
+          <Card icon={<IoAccessibility className='size-12'/>}  title='Accessibility Features' text='Our platform is designed to be inclusive and accessible for all users.'/>
         </div>
       </div>
     </div>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { TbDeviceUnknownFilled } from "react-icons/tb";
+import { twMerge } from "tailwind-merge";
 
 export default function Card({ icon = <TbDeviceUnknownFilled className="size-12"/>, iconBackgroundColor = '#6B7280', title, text, className = '' }) {
   return (
-    <div className={`w-[384px] bg-[#F9FAFB] px-6 py-8 text-center rounded-lg relative hover:drop-shadow-sm ${className}`}>
-      <div className="absolute top-[-24px] left-[50%] transform -translate-x-1/2" style={{ color: iconBackgroundColor }}>{icon}</div>
-      <h2 className="text-[#111827] pt-8 pb-5">{title}</h2>
-      <p className="text-[#6B7280] font-normal leading-relaxed">{text}</p>
+    <div className={
+      twMerge(`w-96 bg-gray-50 px-6 py-8 text-center rounded-lg relative hover:drop-shadow-sm ${className}`)}>
+      <div className="absolute -top-6 left-1/2 transform -translate-x-1/2 text-gray-500" style={{ color: iconBackgroundColor }}>{icon}</div>
+      <h2 className="text-gray-900 pt-8 pb-5">{title}</h2>
+      <p className="text-gray-500 font-normal leading-relaxed">{text}</p>
     </div>
   );
 }
