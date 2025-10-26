054fe5d5dd5982a18cbaa3a96bac7537bec97cb4
bienvenudev <cbienvenu007@gmail.com>
2025-10-26 13:32:43 +0200

    feat: update styling and layout for TestimonialWithImage component

--- src/components/Testimonials/TestimonialWithImage.jsx
+++ src/components/Testimonials/TestimonialWithImage.jsx
@@ -4,11 +4,15 @@ import { BsQuote } from "react-icons/bs";
 export default function TestimonialWithImage({ image, quote, name, role }) {
   console.log("image is", image.type);
   return (
-    <div className="border-4 py-24 relative">
+    <div className="border-4 pt-28 bg-white lg:bg-inherit lg:py-24 relative">
       <div className="bg-blue-800">
-        <div className=" flex text-white px-10 ml-[455px]">
-          <img className="absolute left-16 top-1/2 -translate-y-1/2 rounded-xl" src={image.type} width="384" alt="" />
-          <div className="py-20 px-8 max-w-[800px]">
+        <div className="text-white px-4 lg:px-10 lg:ml-[455px]">
+          <img
+            className="absolute max-h-[206px] w-xs rounded-xl top-0 object-cover object-[50%_25%] left-1/2 -translate-x-1/2 lg:max-h-full lg:w-full lg:max-w-sm lg:left-65 lg:top-1/2 lg:-translate-y-1/2"
+            src={image.type}
+            alt=""
+          />
+          <div className="pt-40 pb-16 lg:py-20 px-8 max-w-xs lg:max-w-[800px]">
             <BsQuote className="text-4xl" />
             <div className="py-6 text-2xl font-medium leading-8">{quote}</div>
             <div className="font-bold leading-6">{name}</div>
